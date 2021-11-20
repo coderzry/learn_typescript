4bcf107dd7e485af54d748a47df1f830c1b4ea35
coderzry <18348738308@163.com>
2021-11-20 21:42:04 +0800

    typescript邂逅

---
+++ 02_邂逅TypeScript/01_Hello_TypeScript.js
@@ -0,0 +1,7 @@
+var message = 'hello, typescript';
+function foo(payload) {
+    console.log(payload.length);
+}
+
+// foo(123);
+foo('aaa')
---
+++ 02_邂逅TypeScript/01_Hello_TypeScript.ts
@@ -0,0 +1,9 @@
+let message: string = 'hello, typescript'
+
+function foo(payload: string) {
+  console.log(payload.length);
+}
+
+// foo(123)
+foo('aaa')
+
